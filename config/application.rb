fcfe443d6a3143cbad128f1772824e46f9923e11
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2016-06-03 16:48:23 +0000

    start heroku

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module AwesomeEvent
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = false
+    config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
